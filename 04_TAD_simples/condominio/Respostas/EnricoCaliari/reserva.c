86980aa57a58d113c24576f6f3c91eede51d9440
Enrico M Caliari <enricomassariol@gmail.com>
2024-12-23 19:41:00 -0300

    Teste

--- 04_TAD_simples/condominio/Respostas/EnricoCaliari/reserva.c
+++ 04_TAD_simples/condominio/Respostas/EnricoCaliari/reserva.c
@@ -9,20 +9,3 @@ Reserva criaReserva(Morador morRes, Area aRes, Data dRes, int qtdConvidados)
     reserva.qtdConvidados = qtdConvidados;
     return reserva;
 }
-
-int verificaSolicitacaoReserva(Reserva *agendaReservas, int nRes, Morador morRes, Area aRes, Data dRes, int qtdConvidados)
-{
-}
-
-void imprimeReserva(Reserva r)
-{
-    printf("--------- RESERVA -----------\n");
-    printf("Morador:\n");
-    imprimeMorador(r.morador);
-    printf("\n");
-    printf("Area reservada:\n");
-    imprimeArea(r.area);
-    printf("\n");
-    printf("Quantidade de convidados: %d\n", r.qtdConvidados);
-    printf("Taxa de ocupacao: %d%%\n", r.qtdConvidados / r.area.capacidade * 100);
-}
