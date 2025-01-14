24fa34fa4a2c0676be101e6294ff318307f5e926
Enrico M Caliari <enricomassariol@gmail.com>
2025-01-14 00:12:56 -0300

    TAD_00 feito

---
+++ 04_TAD_simples/TAD_00/Respostas/EnricoCaliari/ponto.c
@@ -0,0 +1,42 @@
+#include "ponto.h"
+#include <math.h>
+
+Ponto pto_cria(float x, float y)
+{
+    Ponto p;
+    p.x = x;
+    p.y = y;
+    return p;
+}
+
+float pto_acessa_x(Ponto p)
+{
+    return p.x;
+}
+
+float pto_acessa_y(Ponto p)
+{
+    return p.y;
+}
+
+Ponto pto_atribui_x(Ponto p, float x)
+{
+    p.x = x;
+    return p;
+}
+
+Ponto pto_atribui_y(Ponto p, float y)
+{
+    p.y = y;
+    return p;
+}
+
+float pto_distancia(Ponto p1, Ponto p2)
+{
+    float horizontal = p1.x - p2.x;
+    float vertical = p1.y - p2.y;
+
+    float distancia = sqrt(pow(horizontal, 2) + pow(vertical, 2));
+    
+    return distancia;
+}
